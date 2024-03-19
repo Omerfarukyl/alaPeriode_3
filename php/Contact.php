d13835308dc565b30e2a304ab8b45ac98d11ab61
Thimo kamp <150125005+Thimokamp@users.noreply.github.com>
2024-03-19 10:30:54 +0100

    contactpage

---
+++ php/Contact.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage</title>
+
+    <link rel="stylesheet" href="../style/style.css">
+
+</head>
+<body style="font-family: -apple-system, BlinkMacSystemFont, segoe ui, roboto, oxygen, ubuntu, cantarell, fira sans, droid sans, helvetica neue, Arial, sans-serif;">
+
+    <div class="navbar">
+        <a href="home.php">HomePage</a>
+        <a href="products.php">Producten</a>
+        <a href="index2.php?page=cart">Winkelwagentje</a>
+        <a href="InloggegevensPage.php">Mijn account</a>
+        <a href= "contact.php">Contact</a>
+    </div>
+  <div class="container">
+  <img src="img/call2.png" alt="call us" style="width:128px;height:128px;">
+</div>
+
+  
+
+</html>
