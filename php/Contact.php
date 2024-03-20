c253c7640afaa6d6a57d6e1217edcd6e4a201b69
Thimo kamp <150125005+Thimokamp@users.noreply.github.com>
2024-03-20 13:27:25 +0100

    Update Contact.php

--- php/Contact.php
+++ php/Contact.php
@@ -18,9 +18,12 @@
         <a href="InloggegevensPage.php">Mijn account</a>
         <a href= "contact.php">Contact</a>
     </div>
-  <div class="container">
-  <img src="img/call2.png" alt="call us" style="width:128px;height:128px;">
+
+    <a href=""><img src="../img/call.png"  alt="bellen" style="width:165px;height:165px;margin-left:20%; margin-top:17%">
+    <a href="mailto:6036553@mborijnland.nl">Email<img src="../img/mail.png"  alt="email" style="width:165px;height:165px;margin-left:15%; margin-top:17%">
+    <a href=""><img src="../img/"  alt="Form" style="width:165px;height:165px;margin-left:15%; margin-top:17%">
 </div>
+
 
   
 
