0230472dd1ebbdbe618c437142c00d55a1e8635c
Thimo kamp <150125005+Thimokamp@users.noreply.github.com>
2024-04-03 13:03:33 +0200

    Update Contact.php

--- php/Contact.php
+++ php/Contact.php
@@ -5,26 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>homepage</title>
-
-    <link rel="stylesheet" href="../style/style.css">
-
+    <link rel="stylesheet" href="../style/contact.css">
 </head>
 <body style="font-family: -apple-system, BlinkMacSystemFont, segoe ui, roboto, oxygen, ubuntu, cantarell, fira sans, droid sans, helvetica neue, Arial, sans-serif;">
 
-    <div class="navbar">
-        <a href="home.php">HomePage</a>
-        <a href="products.php">Producten</a>
-        <a href="index2.php?page=cart">Winkelwagentje</a>
-        <a href="InloggegevensPage.php">Mijn account</a>
-        <a href= "contact.php">Contact</a>
-    </div>
+<div class="navbar">
+    <a href="home.php">HomePage</a>
+    <a href="products.php">Producten</a>
+    <a href="index2.php?page=cart">Winkelwagentje</a>
+    <a href="InloggegevensPage.php">Mijn account</a>
+    <a href="contact.php">Contact</a>
+</div>
 
-    <a href=""><img src="../img/call.png"  alt="bellen" style="width:165px;height:165px;margin-left:20%; margin-top:17%">
-    <a href="mailto:6036553@mborijnland.nl">Email<img src="../img/mail.png"  alt="email" style="width:165px;height:165px;margin-left:15%; margin-top:17%">
-    <a href=""><img src="../img/"  alt="Form" style="width:165px;height:165px;margin-left:15%; margin-top:17%">
+<div>
+    <a href=""><img src="../img/call.png" alt="bellen" style="width:165px;height:165px;margin-left:30%; margin-top:17%;"></a>
+    <a href="mailto:6036553@mborijnland.nl"><img src="../img/mail.png" alt="email" style="width:165px;height:165px;margin-left:15%; margin-top:17%;"></a>
+   
+
+
 </div>
 
 
-  
+
+
+<footer class="footer2">
+    <p>further information
+
+    </p>
+</footer>
+
+</body>
 
 </html>
+
+
+
